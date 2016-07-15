502412953e9ad3b19a98ed19ecf0754552d0123e
Daniel Jamrozik <djamro2@illinois.edu>
2016-07-14 23:59:15 -0700

    final front end changes

--- app/controllers/LandingPageController.js
+++ app/controllers/LandingPageController.js
@@ -3,20 +3,52 @@
 'use strict';
 
 app.controller('LandingPageController', ['$scope', '$window', function($scope, $window){
-
     var vm = this;
     $scope.positionSelected = 'promoters';
+    $scope.application = {};
+    $scope.applicationSent = false;
 
     // this function is called on start up, get the necessary data here
     vm.init = function(){
         Parse.initialize("tkItk2kkSyvQtkyaRmImxGC6ZPNXYPZCenkvgbuQ", "teE9Ak1WfrKgp1Cyzyi5yKnUN45cVOWojmWr1h6O");
     };
 
+    // route the user to the correct event on docksocial
     $scope.goToEvent = function(event) {
         // if no event given, go to docksocial.com
         if (!event) {
             $window.location.href = 'https://www.docksocial.com';
         }
+    };
+
+    // save the users application, call cloud code function to send the email
+    $scope.saveApplication = function(form) {
+        var Application = Parse.Object.extend("Application");
+        var application = new Application();
+
+        application.set("Name", form.name);
+        application.set("College", form.college);
+        application.set("Email", form.email);
+        application.set("PhoneNumber", form.phoneNumber);
+        application.set("Info", form.info);
+
+        application.save({
+            success: function(result) {
+                Parse.Cloud.run('sendApplicationEmail', form, {
+                    success: function(result) {
+                        $scope.applicationSent = true;
+                        $scope.applicationError = false;
+                        $scope.$apply();
+                    },
+                    error: function(error) {
+                        console.error('Error: ' + error);
+                    }
+                })
+            },
+            error: function(error) {
+                console.error('Error saving the application: ' + error);
+            }
+        });
     };
 
     vm.init();
